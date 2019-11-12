18620a646bbe5defac91da42e460342242a2d1f9
Ty Lippe <tylippe94@gmail.com>
2019-11-12 13:52:11 -0500

    user model and user router

---
+++ api/user/userModel.js
@@ -0,0 +1,35 @@
+const db = require('../../data/dbConfig')
+
+module.exports = {
+    findAll,
+    findById,
+    addUser,
+    deleteUser,
+    editUser
+}
+
+function findAll() {
+    return db('Users')
+}
+
+function findById(id) {
+    return db('Users')
+    .where({ id })
+}
+
+function addUser(user) {
+    return db('Users')
+    .insert(user)
+}
+
+function deleteUser(id) {
+    return db('Users')
+    .where({ id })
+    .del()
+}
+
+function editUser(id, changes) {
+    return db('Users')
+    .where({ id })
+    .update(changes)
+}
