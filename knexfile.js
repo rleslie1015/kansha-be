2124a8fd68a9cb3fd2cc4cc804fb6933fefe3f96
Matt Masters <Matt.d.m@hotmail.com>
2019-11-13 16:06:19 -0600

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -18,35 +18,33 @@ module.exports = {
     }
   },
 
-  staging: {
+  development: {
     client: 'pg',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
     },
     pool: {
       min: 2,
       max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
     }
   },
 
-  production: {
+  development: {
     client: 'pg',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
     },
     pool: {
       min: 2,
       max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
     }
   }
 
