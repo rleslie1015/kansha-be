451c71b8f06e7b050d427263d5911c5189a33d61
Matt Masters <Matt.d.m@hotmail.com>
2019-11-13 16:03:52 -0600

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: process.env.DB_URL,
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './data/migrations'
     },
