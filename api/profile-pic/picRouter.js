c1f7c977bf77092110bbee0011b981bf314960bb
leslie <rleslie1015@gmail.com>
2019-11-20 01:07:31 -0600

    changed the jason response from profile pic url to just url

--- api/profile-pic/picRouter.js
+++ api/profile-pic/picRouter.js
@@ -6,7 +6,8 @@ const singleUpload = upload.single('image');
 router
     .post('/', function(req, res) {
         singleUpload(req, res, function(err) {
-            return res.json({'profile pic url': req.file.location});
+            console.log(req.file.location)
+            return res.json({'url': req.file.location});
         }
         )
     });
