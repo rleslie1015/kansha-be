6279fff258683575906991a225b69f7aeb496e7e
Matt M <matt.d.m@hotmail.com>
2019-11-21 17:15:22 -0600

    📁 profile endpoint now sends user intereactions

--- api/recognition/recModel.js
+++ api/recognition/recModel.js
@@ -1,44 +1,70 @@
-const db = require('../../data/dbConfig')
+const db = require('../../data/dbConfig');
 
-module.exports={
-    findAll,
-    findById,
-    deleteRec,
-    editRec,
-    addRec,
-    getUserInteractions
+module.exports = {
+	findAll,
+	findById,
+	deleteRec,
+	editRec,
+	addRec,
+	getUserInteractions,
+};
+
+function findAll() {
+	return db('Recognition');
 }
 
-function findAll(){
-    return db('Recognition')
-}
-
-function findById(id){
-     return db('Recognition')
-    .where({id})
+function findById(id) {
+	return db('Recognition').where({ id });
 }
 
 function deleteRec(id) {
-    return db('Recognition')
-    .where({id})
-    .del()
+	return db('Recognition')
+		.where({ id })
+		.del();
 }
 
-function editRec(id, changes){
-    return db('Recognition')
-    .where({ id })
-    .update(changes)
+function editRec(id, changes) {
+	return db('Recognition')
+		.where({ id })
+		.update(changes);
 }
 
-function addRec(obj, id){
-    return db('Recognition')
-    .insert(obj)
-    .then(() => {
-        findAll()
-    })
+function addRec(obj, id) {
+	return db('Recognition')
+		.insert(obj)
+		.then(() => {
+			findAll();
+		});
 }
 
 function getUserInteractions(id) {
-    return db('Recognition')
-    .where({sender: id})
+	return db
+		.select([
+			'rec.message',
+			'rec.date',
+			's.first_name as sender_first_name',
+			's.last_name as sender_last_name',
+			'r.first_name as recipient_first_name',
+			'r.last_name as recipient_last_name',
+		])
+		.from('Recognition as rec')
+		.where('sender', '=', id)
+		.join('Users as s', 'rec.sender', '=', 's.id')
+		.join('Users as r', 'rec.recipient', '=', 'r.id')
+		.union([
+			db
+				.select([
+					'rec.message',
+					'rec.date',
+					's.first_name as sender_first_name',
+					's.last_name as sender_last_name',
+					'r.first_name as recipient_first_name',
+					'r.last_name as recipient_last_name',
+				])
+				.from('Recognition as rec')
+				.where('recipient', '=', id)
+				.join('Users as s', 'rec.sender', '=', 's.id')
+				.join('Users as r', 'rec.recipient', '=', 'r.id'),
+        ])
+        .orderBy('date');
 }
--- api/user/profileRouter.js
+++ api/user/profileRouter.js
@@ -4,10 +4,11 @@ const auth = require('../../middleware/authMiddleWare');
 
 router.get('/', auth.validateId, (req, res) => {
     console.log(req.profile.id)
-	recModel.getUserInteractions(req.profile.id).then(rec => {
-        console.log(rec)
-    });
-    res.status(200).json({ user: req.profile });
+    recModel.getUserInteractions(req.profile.id).then(rec => {
+        req.profile.rec = rec
+        res.status(200).json({ user: req.profile });
+    })
+    
 });
 
 module.exports = router;
