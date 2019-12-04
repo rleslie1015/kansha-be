c739aaea31b8f1d30250bc7de1a73edd02345b9a
Gustavo Isturiz <isturizgustavo@gmail.com>
2019-12-04 15:35:47 -0500

    added some GET and POST tests to the rec endpoint

--- tests/recRouter.spec.js
+++ tests/recRouter.spec.js
@@ -3,17 +3,49 @@ const server = require('../server');
 const Rec = require('../api/recognition/recModel');
 
 describe('GET /', () => {
-    test.todo('shoulde return status 200'), () => {
-        return request(server).get('/rec/')
-            .expect(200)
-            .then(res => {
-                expect(res.type).toMatch(/json/i);
-            })
-    }
+    it('shoulde return status 200', () => {
+        return request(server)
+            .get('/rec/')
+            .then(response => {
+                expect(response.status).toBe(200);
+            });
+    });
+
+    test('should return JSON', async () => {
+        const response = await request(server).get('/');
+
+        expect(response.type).toMatch(/json/i);
+    });
 });
 
-describe('POST /', () => {
-    test.todo('should add a recognition'), () => {
+describe('GET /:id', () => {
+    it('shoulde return status 200', () => {
+        return request(server)
+            .get('/rec/1')
+            .then(response => {
+                expect(response.status).toBe(200);
+            });
+    });
 
-    }
-})
+    test('should return JSON', async () => {
+        const response = await request(server).get('/');
+
+        expect(response.type).toMatch(/json/i);
+    });
+});
+
+describe('POST /rec', function () {
+    it('responds with json', function (done) {
+        request(server)
+            .post('/rec/')
+            .send({ recipient: 1, sender: 2, message: 'Go be a popcicle!', date: '2019/11/13' })
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(201)
+            .end(function (err, res) {
+                if (err) return done(err);
+                done();
+            });
+    });
+});
+
