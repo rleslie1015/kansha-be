d7455fd7b3745229ef4d9cd683ff37ca88ae73ae
Joscelyn <JoscelynRose@gmail.com>
2020-02-27 16:00:08 -0500

    uploading csv and converting to JSON is working
    Co-authored-by: AndrewMaddocks <andrewmadd@icloud.com>
    Co-authored-by: keveightysev <keveightysev@gmail.com>
    Co-authored-by: Hail91 <aarongillies88@gmail.com>
    Co-authored-by: ackers93 <andrew.ackers@gmail.com>

--- api/csv/csvUploadRouter.js
+++ api/csv/csvUploadRouter.js
@@ -10,20 +10,42 @@ an AWS bucket
 const csv = require('csvtojson');
 const router = require('express').Router();
 const auth = require('../../middleware/authMiddleWare');
+const multer = require('multer');
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 router.use(auth.validateId);
 
-router.get('/', async (req, res) => {
-	const csvFilePath = './api/csv/employeeCSV.csv';
+router.post('/', upload.single('bulkupload'), async (req, res) => {
+	// const csvFilePath = req.file;
 
 	try {
-		const jsonArray = await csv().fromFile(csvFilePath);
-		console.log(jsonArray);
-		return res.status(200).json(jsonArray);
+		const jsonArray = await csv().fromString(req.file.buffer.toString());
+
+		/*
+// create migration to remove notnullable from 'sub' in the user table
+
+        // loop over the array
+            // for each element check if required fields are present
+                // create a user for each one
+                    // add that user to the employee table
+                        // DON'T FORGET to give user_type (standard)
+
+        // if none of the fields work, then return 400 error 
+            //(you need a first_name, last_name, email, department, job_title)
+        
+        // if it worked return a success message, the number of employees added
+        return res.status(200).json(jsonArray);
+        
+        */
 	} catch (error) {
 		console.log(error, 'error');
 		return res.status(500).json(error);
 	}
+
+	// console.log(req.file.buffer.toString());
+	// res.status(200).end();
 });
 
 module.exports = router;
