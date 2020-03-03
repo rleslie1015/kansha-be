7475f27f1d74bc1d4a22b59995402414e40e8cbc
Joscelyn <JoscelynRose@gmail.com>
2020-03-03 17:04:14 -0500

    changed the endpoints so it's just one endpoint where the user passes in the time as a query

--- api/reports/reportModel.js
+++ api/reports/reportModel.js
@@ -2,56 +2,22 @@ const db = require('../../data/dbConfig');
 const moment = require('moment');
 
 module.exports = {
-	oneDayDataForMyOrg,
-	oneWeekDataForMyOrg,
-	oneMonthDataForMyOrg,
-	oneYearDataForMyOrg,
+	dataForMyOrg,
 };
 
-async function oneDayDataForMyOrg(org_id) {
-	let { count } = await db('Recognition')
-		.where({ org_id })
-		.andWhere('date' < new Date(moment('YYYY-MM-DD')))
-		.count()
-		.first();
-	count = Number(count);
+async function dataForMyOrg(org_id, query = {}) {
+	const { time = '' } = query;
 
-	return count;
-}
-
-async function oneWeekDataForMyOrg(org_id) {
 	let { count } = await db('Recognition')
 		.where({ org_id })
 		.andWhere(
-			'date' <
-				new Date(
-					moment()
-						.subtract(7, 'days')
-						.calendar(),
-				),
+			'date',
+			'>',
+			moment()
+				.subtract(1, time)
+				.toDate(),
 		)
-		.count()
-		.first();
-	count = Number(count);
 
-	return count;
-}
-
-async function oneMonthDataForMyOrg(org_id) {
-	let { count } = await db('Recognition')
-		.where({ org_id })
-		.andWhere('date' < new Date(moment('YYYY-MM')))
-		.count()
-		.first();
-	count = Number(count);
-
-	return count;
-}
-
-async function oneYearDataForMyOrg(org_id) {
-	let { count } = await db('Recognition')
-		.where({ org_id })
-		.andWhere('date' < new Date(moment('YYYY')))
 		.count()
 		.first();
 	count = Number(count);
