eb0980fa9940c507c3f04d6c65ea3366d15d8bdc
Joscelyn <JoscelynRose@gmail.com>
2020-03-03 13:50:11 -0500

    made endpoints for the different times

--- api/reports/reportModel.js
+++ api/reports/reportModel.js
@@ -1,23 +1,60 @@
 const db = require('../../data/dbConfig');
+const moment = require('moment');
 
 module.exports = {
-	recognitionInfoForMyOrg,
+	oneDayDataForMyOrg,
+	oneWeekDataForMyOrg,
+	oneMonthDataForMyOrg,
+	oneYearDataForMyOrg,
 };
 
-async function recognitionInfoForMyOrg(org_id) {
-	const recognitionInfo = await db('Recognition')
-		.select('recipient', 'sender', 'date')
-		.where({ org_id })
-		.orderBy('date', 'desc');
-
+async function oneDayDataForMyOrg(org_id) {
 	let { count } = await db('Recognition')
 		.where({ org_id })
+		.andWhere('date' < new Date(moment('YYYY-MM-DD')))
 		.count()
 		.first();
 	count = Number(count);
 
-	return {
-		count,
-		recognitionInfo,
-	};
+	return count;
+}
+
+async function oneWeekDataForMyOrg(org_id) {
+	let { count } = await db('Recognition')
+		.where({ org_id })
+		.andWhere(
+			'date' <
+				new Date(
+					moment()
+						.subtract(7, 'days')
+						.calendar(),
+				),
+		)
+		.count()
+		.first();
+	count = Number(count);
+
+	return count;
+}
+
+async function oneMonthDataForMyOrg(org_id) {
+	let { count } = await db('Recognition')
+		.where({ org_id })
+		.andWhere('date' < new Date(moment('YYYY-MM')))
+		.count()
+		.first();
+	count = Number(count);
+
+	return count;
+}
+
+async function oneYearDataForMyOrg(org_id) {
+	let { count } = await db('Recognition')
+		.where({ org_id })
+		.andWhere('date' < new Date(moment('YYYY')))
+		.count()
+		.first();
+	count = Number(count);
+
+	return count;
 }
--- api/reports/reportRouter.js
+++ api/reports/reportRouter.js
@@ -5,11 +5,50 @@ const auth = require('../../middleware/authMiddleWare');
 router.use(auth.validateId);
 
 // Get all recognitions for the organization
-router.get('/', async (req, res) => {
+router.get('/day', async (req, res) => {
 	const { org_id } = req.profile;
 
 	try {
-		const reportInfo = await reportModel.recognitionInfoForMyOrg(org_id);
+		const reportInfo = await reportModel.oneDayDataForMyOrg(org_id);
+
+		return res.status(201).json(reportInfo);
+	} catch (error) {
+		console.log('error getting report', error);
+		return res.status(500).json({ error });
+	}
+});
+
+router.get('/week', async (req, res) => {
+	const { org_id } = req.profile;
+
+	try {
+		const reportInfo = await reportModel.oneWeekDataForMyOrg(org_id);
+
+		return res.status(201).json(reportInfo);
+	} catch (error) {
+		console.log('error getting report', error);
+		return res.status(500).json({ error });
+	}
+});
+
+router.get('/month', async (req, res) => {
+	const { org_id } = req.profile;
+
+	try {
+		const reportInfo = await reportModel.oneMonthDataForMyOrg(org_id);
+
+		return res.status(201).json(reportInfo);
+	} catch (error) {
+		console.log('error getting report', error);
+		return res.status(500).json({ error });
+	}
+});
+
+router.get('/year', async (req, res) => {
+	const { org_id } = req.profile;
+
+	try {
+		const reportInfo = await reportModel.oneYearDataForMyOrg(org_id);
 
 		return res.status(201).json(reportInfo);
 	} catch (error) {
