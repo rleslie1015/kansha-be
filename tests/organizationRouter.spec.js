06d762b4a79760c79ad1d07175b88e34acd31a6b
Andrew Ackerman <andrew.ackers@gmail.com>
2020-03-04 10:13:21 -0800

    Testing organization endpoints
    Co-authored-by: bamfranceschi <anna.franceschi@gmail.com>

--- tests/organizationRouter.spec.js
+++ tests/organizationRouter.spec.js
@@ -1,29 +1,98 @@
-// const server = require('../server');
-// const request = require('supertest');
-// const Org = require('../api/organization/organizationModel');
-// const testdb = require('../data/dbConfig');
+const server = require('../server');
+const request = require('supertest');
+const testdb = require('../data/dbConfig');
 
-// const auth = require('../middleware/authMiddleWare');
+jest.mock('express-jwt', () => {
+	return jest.fn(() => {
+		return jest.fn((req, res, next) => {
+			req.user = {
+				sub: '1',
+				org_id: 1,
+				email: 'testing_email@kansharewards.com',
+			};
+			next();
+		});
+	});
+});
 
-// jest.mock('../middleware/authMiddleware.js');
-// beforeEach(testdb);
+afterEach(() => {
+	jest.clearAllMocks();
+});
 
-// const token =
-// 	'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlFVUkVSVGhHTnpBek9USXpNelUwTlRKRE1ERkJORGMzT1RFMFFUZEdOVGRFT1RZeVJFSTRSQSJ9.eyJlbWFpbCI6ImFuZHJldy5hY2tlcnNAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImlzcyI6Imh0dHBzOi8va2Fuc2hhLmF1dGgwLmNvbS8iLCJzdWIiOiJnb29nbGUtb2F1dGgyfDEwMjkzNDU4MDU0NjMxMDg2MDk0MyIsImF1ZCI6Ijd6TEVMSEEzSnlydDdPNU01NjFIZ0JnM0VqUnNvVDVLIiwiaWF0IjoxNTgyNjQ1NDczLCJleHAiOjE1ODI2ODE0NzMsImF0X2hhc2giOiJpazVuTVUySURvV2xjcHZqdFFlNG9RIiwibm9uY2UiOiIxMjMxMjQxNDEzIn0.EmK6O2hescYqdJjbMPj34bc1YfFmNw0NoupnMv79wi5X9ICtfTx0Fia3WXj_JsqhXyk2mQYXnRiN7iwnlQF-TO6fQx6XKV7i0PLBZ22NMx6Iwcw0tQcTbutwHOT7PRWWMuztoUKKeit7IzCdEjiJWu6hR3P_XlsN9Eg6e4IfNq1l9-s1ir9lce2gV1liaBVak2MofwfArd0Wo1eHL7Eg_ceJC3rt0q91qv-E1zionoLne3_xJmPRCuyHGQPFi5-NKo4kNhP6jWzm3K3YLaG_vjsxGuy--1ay9l9wSFYClxukPE4K3KrMewZK6q3bwwCigYKKEbmdwpvYxYmjhBazYw';
+beforeAll(() => {
+	return testdb.seed.run();
+});
 
-// // need a way to mock router middleware so that it skips the logic and goes straight to next()
-describe('GET /api/organizations', () => {
-	it.skip('should return status 200', () => {
-		// 	try {
-		// 		// auth.validateToken.mockImplementationOnce((req, res, next) => {
+describe('/organizations router', () => {
+	describe.skip('GET /organizations/', () => {
+		it('return all organizations', async () => {
+			const { body } = await request(server).get('/organizations');
+			expect(body).toEqual(
+				expect.arrayContaining([
+					{
+						id: 1,
+						name: 'Organization 1',
+					},
+				]),
+			);
+		});
+	});
 
-		// 		// 	next();
-		// 		// });
-		console.dir(auth.validateToken);
-		return request(server)
-			.get('/organizations')
-			.set('Authorization', token)
+	//will use middleware
 
-			.expect(200);
+	describe.skip('GET /organizations/:id', () => {
+		it('returns one organization with matching id', async () => {
+			const { body } = await request(server).get('/organizations/1');
+			expect(body).toMatchObject({
+				id: 1,
+				name: 'Organization 1',
+				company_size: null,
+				industry: null,
+				logo_url: null,
+				primary_color: null,
+			});
+		});
+	});
+
+	//will use middleware
+
+	describe.skip('DELETE /organizations', () => {
+		it('should successfully delete an organization by id', async () => {
+			const { status } = await request(server).delete('/organizations/1');
+			expect(status).toBe(204);
+		});
+	});
+
+	describe('POST /organizations', () => {
+		it('should successfully create a new organization', async () => {
+			const { body } = await request(server)
+				.post('/organizations')
+				.send({
+					name: 'Organization 3',
+					company_size: null,
+					industry: null,
+					logo_url: null,
+					primary_color: null,
+				});
+			expect(body.org_id).toBe(3);
+		});
+	});
+
+	//will use middleware
+
+	describe.skip('PUT /organizations', () => {
+		it('should edit a new org successfully', async () => {
+			const { body } = await (
+				await request(server).put('/organizations/2')
+			).send({
+				id: 1,
+				name: 'NEW ORG',
+				company_size: null,
+				industry: null,
+				logo_url: null,
+				primary_color: null,
+			});
+			expect(body).toMatchObject({ id: 1, name: 'NEW ORG' });
+		});
 	});
 });
