75b62c2d952d0d0156d6cfe9c6f5b3a7bcd12fc4
Andrew Ackerman <andrew.ackers@gmail.com>
2020-03-04 16:06:30 -0800

    Testing endpoints. file structure.
    Co-authored-by: bamfranceschi <anna.franceschi@gmail.com>

---
+++ tests/reactionsRouter.spec.js
@@ -0,0 +1,39 @@
+const server = require('../server');
+const request = require('supertest');
+const testdb = require('../data/dbConfig');
+
+jest.mock('express-jwt', () => {
+	return jest.fn(() => {
+		return jest.fn((req, res, next) => {
+			req.user = {
+				sub: '1',
+				org_id: '1',
+				email: 'testing_email@kansharewards.com',
+			};
+			next();
+		});
+	});
+});
+
+afterEach(() => {
+	jest.clearAllMocks();
+});
+
+beforeAll(() => {
+	return testdb.seed.run();
+});
+
+describe('/reactions router', () => {
+	describe.skip('GET /reactions/:rec_id', () => {
+		it('should return one reaction by id', async () => {
+			const { body } = await request(server).get('/reactions/1');
+			expect(body).toBe();
+		});
+	});
+	describe('POST /reactions/', () => {
+		it.todo('should successfully add a reaction to a recognition');
+	});
+	describe('DELETE /reactions/:id', () => {
+		it.todo('should successfully delete a reaction on a recognition');
+	});
+});
