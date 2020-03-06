7e3d85af67d455e6b297b221d4a533eae410602c
Kevin Smith <keveightysev@gmail.com>
2020-03-06 11:48:31 -0600

    skipping uncompleted test

--- tests/routers/picRouter.js
+++ tests/routers/picRouter.js
@@ -2,7 +2,7 @@ const server = require('../../server');
 const request = require('supertest');
 
 module.exports = () => {
-	describe('pic router', () => {
+	describe.skip('pic router', () => {
 		describe('POST /profile-pic', () => {
 			it('post new pic', async () => {
 				const { body } = await request(server).get('./pic');
