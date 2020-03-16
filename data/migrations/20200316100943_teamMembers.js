176e63e9f1e6cdb0b0c2163cabc078903b83f30e
Andrew Maddocks <andrewmadd@icloud.com>
2020-03-16 10:15:44 -0600

    added team and teammembers migrations

---
+++ data/migrations/20200316100943_teamMembers.js
@@ -0,0 +1,22 @@
+exports.up = function(knex) {
+	return knex.schema.createTable('TeamMembers', function(tbl) {
+		tbl.increments();
+		tbl.integer('user_id')
+			.notNullable()
+			.references('id')
+			.inTable('Users')
+			.onDelete('CASCADE')
+			.onUpdate('CASCADE');
+		tbl.integer('team_id')
+			.notNullable()
+			.references('id')
+			.inTable('Teams')
+			.onDelete('CASCADE')
+			.onUpdate('CASCADE');
+		tbl.string('team_role').notNullable();
+	});
+};
+
+exports.down = function(knex) {
+	return knex.schema.dropTableIfExists('TeamMembers');
+};
