d1baf3dee678d13c7bf378fed4152a2a94dcd855
Kevin Smith <keveightysev@gmail.com>
2020-03-18 13:38:40 -0500

    New organization bug fixed

--- api/user/userModel.js
+++ api/user/userModel.js
@@ -50,13 +50,15 @@ async function addUser(newUser) {
 		sub,
 		department,
 	} = newUser;
-	let { id: org } = await db('Organizations')
+	let org = await db('Organizations')
 		.where({ name: org_name })
 		.first();
 	if (!org) {
-		[org] = await db('Organizations').insert({ name: org_name }, 'id');
+		[org] = await db('Organizations').insert({ name: org_name }, [
+			'id',
+			'name',
+		]);
 	}
-	console.log(org);
 	const [user] = await db('Users').insert(
 		{
 			first_name,
@@ -74,7 +76,7 @@ async function addUser(newUser) {
 	}
 
 	await db('Employees').insert({
-		org_id: org,
+		org_id: org.id,
 		user_id: user,
 		job_title,
 		user_type,
