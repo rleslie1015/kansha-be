fa78ee26ed00e52a5f9a0b97180f5cf56bf1b313
Kevin Smith <keveightysev@gmail.com>
2020-03-28 14:27:43 -0500

    cleaned up reactions routers

--- api/reactions/commentRouter.js
+++ api/reactions/commentRouter.js
@@ -1,59 +1,55 @@
-const dbModel = require('./reactionModel');
+const Comments = require('./reactionModel');
 const { emitterInput } = require('../livefeed/liveFeedEmitter');
-
 const router = require('express').Router();
-router.get('/:rec_id', (req, res) => {
+
+router.get('/:rec_id', async (req, res) => {
 	const { rec_id } = req.params;
-
-	dbModel
-		.getComments(rec_id)
-		.then(comments => {
-			if (!comments) {
-				res.status(404).json({ message: 'post not found' });
-			} else {
-				res.status(200).json(comments);
-			}
-		})
-		.catch(err => {
-			console.error(err);
-			res.status(500).json(err);
-		});
+	try {
+		const comments = await Comments.getComments(rec_id);
+		if (comments) {
+			res.status(200).json(comments);
+		} else {
+			res.status(404).json({ message: 'post not found' });
+		}
+	} catch (err) {
+		console.error(err);
+		res.status(500).json(err);
+	}
 });
 
-router.post('/', (req, res) => {
-	dbModel
-		.addComment({ user_id: req.profile.id, ...req.body })
-		.then(([comment]) => {
-			emitterInput.emit('event', {
-				payload: comment,
-				type: 'FEED_EVENT_NEW_COMMENT',
-				org_name: req.profile.org_name,
-			});
-			res.status(201).json(comment);
-		})
-		.catch(err => {
-			console.error(err);
-			res.status(500).json(err);
+router.post('/', async (req, res) => {
+	try {
+		const [comment] = await Comments.addComment({
+			user_id: req.profile.id,
+			...req.body,
 		});
+		emitterInput.emit('event', {
+			payload: comment,
+			type: 'FEED_EVENT_NEW_COMMENT',
+			org_name: req.profile.org_name,
+		});
+		res.status(201).json(comment);
+	} catch (err) {
+		console.error(err);
+		res.status(500).json(err);
+	}
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
 	const { id } = req.params;
 	const { rec_id } = req.query;
-	dbModel
-		.deleteEvent('Comments', id)
-		.then(() => {
-			emitterInput.emit('event', {
-				payload: { id, rec_id },
-				type: 'FEED_EVENT_REMOVE_COMMENT',
-				org_name: req.profile.org_name,
-			});
-			res.sendStatus(204);
-		})
-		.catch(err => {
-			console.error(err);
-			res.status(500).json(err);
+	try {
+		await Comments.deleteEvent('Comments', id);
+		emitterInput.emit('event', {
+			payload: { id, rec_id },
+			type: 'FEED_EVENT_REMOVE_COMMENT',
+			org_name: req.profile.org_name,
 		});
+		res.sendStatus(204);
+	} catch (err) {
+		console.error(err);
+		res.status(500).json(err);
+	}
 });
 
 module.exports = router;
--- api/reactions/reactionRouter.js
+++ api/reactions/reactionRouter.js
@@ -1,57 +1,55 @@
-const dbModel = require('./reactionModel');
+const Reactions = require('./reactionModel');
 const { emitterInput } = require('../livefeed/liveFeedEmitter');
-
 const router = require('express').Router();
-router.get('/:rec_id', (req, res) => {
+
+router.get('/:rec_id', async (req, res) => {
 	const { rec_id } = req.params;
-
-	dbModel
-		.getReactions(rec_id)
-		.then(reactions => {
-			if (!reactions) {
-				res.status(404).json({ message: 'reactions not found' });
-			} else {
-				res.status(200).json(reactions);
-			}
-		})
-		.catch(err => {
-			res.status(500).json(err);
-		});
+	try {
+		const reactions = await Reactions.getReactions(rec_id);
+		if (reactions) {
+			res.status(200).json(reactions);
+		} else {
+			res.status(404).json({ message: 'reactions not found' });
+		}
+	} catch (err) {
+		console.error(err);
+		res.status(500).json(err);
+	}
 });
 
-router.post('/', (req, res) => {
-	dbModel
-		.addReaction({ user_id: req.profile.id, ...req.body })
-		.then(([reaction]) => {
-			emitterInput.emit('event', {
-				payload: reaction,
-				type: 'FEED_EVENT_NEW_REACTION',
-				org_name: req.profile.org_name,
-			});
-			res.status(201).json(reaction);
-		})
-		.catch(err => {
-			console.log(err);
-			res.status(500).json(err);
+router.post('/', async (req, res) => {
+	try {
+		const [reaction] = await Reactions.addReaction({
+			user_id: req.profile.id,
+			...req.body,
 		});
+		emitterInput.emit('event', {
+			payload: reaction,
+			type: 'FEED_EVENT_NEW_REACTION',
+			org_name: req.profile.org_name,
+		});
+		res.status(201).json(reaction);
+	} catch (err) {
+		console.error(err);
+		res.status(500).json(err);
+	}
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
 	const { id } = req.params;
 	const { rec_id } = req.query;
-	dbModel
-		.deleteEvent('Reactions', id)
-		.then(() => {
-			emitterInput.emit('event', {
-				payload: { id, rec_id },
-				type: 'FEED_EVENT_REMOVE_REACTION',
-				org_name: req.profile.org_name,
-			});
-			res.sendStatus(204);
-		})
-		.catch(err => {
-			res.status(500).json(err);
+	try {
+		await Reactions.deleteEvent('Reactions', id);
+		emitterInput.emit('event', {
+			payload: { id, rec_id },
+			type: 'FEED_EVENT_REMOVE_REACTION',
+			org_name: req.profile.org_name,
 		});
+		res.sendStatus(204);
+	} catch (err) {
+		console.error(err);
+		res.status(500).json(err);
+	}
 });
 
 module.exports = router;
