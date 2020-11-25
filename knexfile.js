67bf4cf64fa6890c7183833f3c337df39c214f8b
Leslie Rodriguez <rleslie1015@gmail.com>
2020-11-24 23:16:23 -0600

    new_db

--- knexfile.js
+++ knexfile.js
@@ -9,6 +9,10 @@ module.exports = {
       directory: './data/migrations',
     },
     seeds: { directory: './data/seeds' },
+  	pool: {
+			min: 2,
+			max: 10,
+		},
   },
 
   staging: {
@@ -18,6 +22,10 @@ module.exports = {
       directory: './data/migrations',
     },
     seeds: { directory: './data/seeds' },
+  	pool: {
+			min: 2,
+			max: 10,
+		},
   },
 
   production: {
@@ -27,5 +35,31 @@ module.exports = {
       directory: './data/migrations',
     },
     seeds: { directory: './data/seeds' },
+    pool: {
+			min: 2,
+			max: 10,
+		},
   },
+  testing: {
+		client: 'pg',
+		connection: {
+			host: process.env.TEST_DB_HOST,
+			user: process.env.TEST_DB_USER,
+			password: process.env.TEST_DB_PASS,
+			database: 'kansha_test',
+			options: {
+				port: process.env.TEST_DB_PORT,
+			},
+		},
+		migrations: {
+			directory: './data/migrations',
+		},
+		seeds: {
+			directory: './tests/seeds',
+		},
+		pool: {
+			min: 2,
+			max: 75,
+		},
+	},
 };
